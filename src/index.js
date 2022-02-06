614da96bc13372e9b51cd02e2df410d4e12a9033
King_of_Undead <victorvsgaming@gmail.com>
2022-02-06 01:51:40 -0600

    got template and ghost types

---
+++ src/index.js
@@ -0,0 +1,70 @@
+let User = '';
+let ghostType = '';
+let ghostRoom = '';
+// 
+function getGhostType() {
+    switch (ghostType) {
+        case "Shade":
+            
+            break;
+        case "Banshee":
+
+            break;
+        case "Polterguist":
+
+            break;
+        case "Jinn":
+
+            break;
+        case "Mare":
+
+            break;
+        case "Demon":
+
+            break;
+        case "Yokai":
+
+            break;
+        case "Myling":
+
+            break;
+        case "Raiju":
+            break;
+
+        case "Wraith":
+            break;
+        case "Revenant":
+            break;
+        case "Yurei":
+            break;
+
+        case "Hantu":
+            break;
+        case "Obake":
+            break;
+        case "Phantom":
+            break;
+        case "Oni":
+            break;
+        case "Goryo":
+            break;
+        case "The Twins":
+            break;
+        case "The Mimic":
+            break;
+    }
+    return ghostType;
+}
+function getGhostRoom() {
+    switch (ghostRoom) {
+        case value:
+            
+            break;
+    
+    }
+    return ghostRoom;
+}
+
+$(document).ready(function () {
+    
+});
